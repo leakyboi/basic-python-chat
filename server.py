875a653501cb349b2cdbd91095c74530ce11bbfc
Drafti.sh <67260470+nexxydev@users.noreply.github.com>
2021-10-19 11:08:37 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
-"""Server for multithreaded (asynchronous) chat application."""
+"""Some setup stuff."""
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
 
 def accept_incoming_connections():
-    """Sets up handling for incoming clients."""
+    """sets up the actual server.."""
     while True:
         client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        print("%s:%s has joined the chatroom." % client_address)
+        client.send(bytes("Greetings from the wierd place. Now type your name and press enter!", "utf8"))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
 
 def handle_client(client):  # Takes client socket as argument.
-    """Handles a single client connection."""
+    """client connection stuff (ONLY HANDLES ONE!)."""
 
     name = client.recv(BUFSIZ).decode("utf8")
     welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
